9d2cc5ff95f35344d65d352e6752a39b1f32d213
Drepp27 <47632903+Drepp27@users.noreply.github.com>
2019-05-10 12:57:24 +0300

    comments added

--- gamelogic_v2.js
+++ gamelogic_v2.js
@@ -1,13 +1,16 @@
+//Function to randomise computer choice
 function getMachineInput() {
     var choices = ["r", "p", "s"];
     var rng = Math.floor(Math.random()*3);
     return choices[rng];
 }
- 
+
+//Player1&2 and machine choices
 var machineInput = getMachineInput();
 var player1choice = ["r", "p", "s"];
 var player2choice = ["r", "p", "s"];
 
+//Compare player1, player2 or machine answers together
 function compare(choice1, choice2) {
 switch(choice1 + choice2) {
     case "rs":
@@ -29,4 +32,5 @@ switch(choice1 + choice2) {
 
 }
 
+//init comparison
 compare(player1choice[0], machineInput)
