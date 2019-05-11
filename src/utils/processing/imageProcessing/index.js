d15662245ecd9409f6b7e0ccd3def1b1f60567ce
devendrk <katwaldevendra@hotmail.com>
2019-05-11 11:22:26 +0300

    tested ml5 library and used random image to check it works or not

--- src/utils/processing/imageProcessing/index.js
+++ src/utils/processing/imageProcessing/index.js
@@ -1,1 +1,13 @@
-// test
+// select the 
+const getImage = document.querySelector('#image');
+const result = document.querySelector('#result');
+
+ // Initialize the Image Classifier method with MobileNet
+ const classifier = ml5.imageClassifier('MobileNet', function() {
+    console.log('Model Loaded!');
+  });
+
+  // Make a prediction with the selected image
+  classifier.predict(image, function(err, results) {
+      result.innerText = results[0].className;
+  });
