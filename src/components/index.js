3fd6f37c8a68c31ab7b9bd7775350f490608b4a6
K C Gaurab <s1900524@edu.bc.fi>
2019-05-11 18:47:12 +0300

    play button added

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
 //code goes here
 setState = 3;
 
-function countdown() {
-	setState--;
-	document.getElementById("seconds").setState = timeleft;
-	if (timeleft > 0) {
+function countdown(a) {
+	a--;
+	// document.getElementById("seconds").setState = timeleft;
+	if (a > 0) {
 		setTimeout(countdown, 1000);
+		console.log(countdown);
 	}
 };
 
-setTimeout(countdown, 1000);
+//setTimeout(countdown, 1000);
+countdown(setState);
