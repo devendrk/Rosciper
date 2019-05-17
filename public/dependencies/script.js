22d74d02052f02346646e32aa9eed9e338374e92
avophile <douglas.locklin@gmail.com>
2019-05-18 02:15:12 +0300

    adds machine strategy options and defaults to Spock

--- public/dependencies/script.js
+++ public/dependencies/script.js
@@ -59,31 +59,35 @@ function playPaper(num){
 
 //Compare player1, player2 or machine answers together
 function compare(choice1, choice2) {
-console.log(choice1 + choice2);
-switch(choice1 + choice2) {
-    case "rs":
-    case "pr":
-    case "sp":
-    document.getElementById("countdown").innerHTML = "You win!";
-    
-    console.log("Player 1 Wins!");
-    breasetTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);k;
-    case "rp":
-    case "ps":
-    case "sr":
-    document.getElementById("countdown").innerHTML = "You lose!";
-    setTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);
-    console.log("Player 2 Wins!");
-    break;
-    case "rr":
-    case "pp":
-    case "ss":
-    document.getElementById("countdown").innerHTML = "It's a tie!";
-    setTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);
-    console.log("Draw!");
-    break;
-}
-
+    if (choice2 == "Spock"){
+        document.getElementById("countdown").innerHTML = "Spock refutes you loser!";
+        console.log("Player 1 Loses! SPock always wins!");
+    } else {
+            console.log(choice1 + choice2);
+            switch(choice1 + choice2) {
+                case "rs":
+                case "pr":
+                case "sp":
+                document.getElementById("countdown").innerHTML = "You win!";
+                
+                console.log("Player 1 Wins!");
+                breasetTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);k;
+                case "rp":
+                case "ps":
+                case "sr":
+                document.getElementById("countdown").innerHTML = "You lose!";
+                setTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);
+                console.log("Player 2 Wins!");
+                break;
+                case "rr":
+                case "pp":
+                case "ss":
+                document.getElementById("countdown").innerHTML = "It's a tie!";
+                setTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);
+                console.log("Draw!");
+                break;
+            }
+    }
 }
 
 //init comparison
